4d0209989b8103f14d1af121b59d69db5999bfa3
dinnertime <dinnertime.dev@gmail.com>
2023-11-29 20:00:50 +0900

    사이트 이름 변경

--- src/configs/sites.ts
+++ src/configs/sites.ts
@@ -1,5 +1,5 @@
 export const siteConfigs = {
-  siteName: 'links@ - 보여줄게 많은 당신을 위해',
+  siteName: 'Links@ - 보여줄게 많은 당신을 위해',
   description: '나만의 컨텐츠를 만들고 사람들에게 알리세요!!',
   author: {
     name: 'at-dinner-time',
