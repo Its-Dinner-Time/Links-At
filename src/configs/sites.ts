414769843302d1b3a367ec548887249eb9b5ea08
dinnertime <dinnertime.dev@gmail.com>
2023-11-29 19:57:34 +0900

    로고추가, 사이트 내용 변경

---
+++ src/configs/sites.ts
@@ -0,0 +1,8 @@
+export const siteConfigs = {
+  siteName: 'links@ - 보여줄게 많은 당신을 위해',
+  description: '나만의 컨텐츠를 만들고 사람들에게 알리세요!!',
+  author: {
+    name: 'at-dinner-time',
+    link: 'https://github.com/Its-Dinner-Time?tab=repositories',
+  },
+};
